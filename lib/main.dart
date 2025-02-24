d5de905644a47dd5a1371905fad974c19e894dfe
Jose Perez <joseperu2503@gmail.com>
2025-02-24 00:25:24 -0500

    styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:neura_app/app/app.dart';
+import 'package:neura_app/app/core/constants/app_colors.dart';
 import 'package:neura_app/app/core/constants/environment.dart';
 import 'package:neura_app/di.dart';
 
 void main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: AppColors.dark2,
+    ),
+  );
+
   await Environment.initEnvironment();
   setup(); // Inicializar inyección de dependencias
   runApp(const App());
